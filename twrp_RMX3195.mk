0bc3dd05c37115723b675a9126618e7884b59736
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2022-08-12 12:17:20 +0700

    Update twrp_RMX3195.mk

--- twrp_RMX3195.mk
+++ twrp_RMX3195.mk
@@ -22,7 +22,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from TWRP common configurations
 $(call inherit-product, vendor/twrp/config/common.mk)
