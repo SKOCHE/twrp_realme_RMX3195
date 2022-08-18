9d84cfde678b9b13db5a7525eb8803545cd2189b
lopestorm <guillopes2002@gmail.com>
2022-08-18 00:00:59 -0300

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2021-2022 TeamWin Recovery Project
+# Copyright (C) 2021 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- twrp_RMX3195.mk
+++ twrp_RMX3195.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2021-2022 TeamWin Recovery Project
+# Copyright (C) 2021 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,44 +13,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-LOCAL_PATH := device/realme/RMX3195
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+	$(LOCAL_PATH)/prebuilt/dtb:dtb.img
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3195
 PRODUCT_NAME := twrp_RMX3195
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3195
 PRODUCT_MANUFACTURER := realme
 
-# Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 31
-# Shipping VNDK level
-PRODUCT_TARGET_VNDK_VERSION := 31
-
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Extra required packages
+PRODUCT_PACKAGES += \
+    libion
+    
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
-
-# Bypass anti-rollback ROMs protection
-# Set build date to Jan 1 2009 00:00:00
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.date.utc=1230768000 \
-    ro.bootimage.build.date.utc=0
-#    ro.build.date.utc=0
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
-    ro.product.device \
-    ro.product.name
