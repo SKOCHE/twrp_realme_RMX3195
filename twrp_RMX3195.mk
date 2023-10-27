953fa94d9961f31f4b5e5372a5ccf2eaeff9ce71
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-10-27 08:35:17 +0700

    Update twrp_RMX3195.mk

--- twrp_RMX3195.mk
+++ twrp_RMX3195.mk
@@ -40,7 +40,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 # Bypass anti-rollback ROMs protection
